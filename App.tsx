6025cfefbe365aa172040fe4d4f6bb3cc1bcab54
Lucas Fagundes <lucasfagundesice@gmail.com>
2025-08-04 22:35:19 -0300

    aula 04/08

--- App.tsx
+++ App.tsx
@@ -1,45 +1,10 @@
+
 import { StyleSheet, Text, View } from 'react-native';
-import {Home} from './src/screens/Home';
+import Home from './src/screens/Home';
 
 export default function App() {
   return (
       <Home />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1.8,
-    backgroundColor: '#912af1',
-    padding: 20,
-    fontSize: 20,
-    alignItems: 'flex-start',
-    textAlign: 'justify',
-    justifyContent: 'flex-end',
-  },
-  bottom: {
-    flex: 9,
-    backgroundColor: '#ffffff',
-    padding: 10,
-    fontSize: 50,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start'
-  },
-  fonte: {
-    color: '#ffffff',
-    fontSize: 40,
-    padding: 0,
-    fontWeight: '700'
-  },
-  fonte2: {
-    fontSize: 35,
-    fontWeight: '600',
-    paddingLeft: 10
-  },
-  fonte3: {
-    fontSize: 18,
-    fontWeight: '700',
-    color: '#fff'
-  }
-
-});
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,70 @@
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 
-export function Home(){
+export default function Home(){
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Lista de Compras</Text>
+            <Text style={styles.todaysDate}>Quarta-feira, 17 de Julho de 2025</Text>
+            <View style={styles.form}>
+                <TextInput 
+                style={styles.input} 
+                placeholder='Nome do Produto'
+                placeholderTextColor="#BDBABA"
+                />
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.textButton}>+</Text>
+                </TouchableOpacity>
+            </View>
         </View>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#EAEAEA",
+        padding: 24,
+    },
+    title: {
+        color: "#000",
+        fontFamily: "Inter",
+        fontSize: 24,
+        fontWeight: "bold",
+        lineHeight: 28.8,
+        marginTop: 48
+    },
+    todaysDate: {
+        color: "#000",
+        fontFamily: "Inter",
+        fontSize: 16,
+        fontWeight: "normal",
+        lineHeight: 28.8,
+    },
+    input: {
+        width: "80%",
+        height: 56,
+        padding: 16,
+        backgroundColor: "#fff",
+        borderRadius: 5
+    },
+    form: {
+        width: "100%",
+        flexDirection: "row",
+        marginTop: 18
+    },
+    button: {
+        marginLeft: 16,
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: "#31C667",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    textButton: {
+        color: "#fff",
+        fontSize: 24,
+    }
+});
